94f6bbcbe7d2c2c8dd85cf4e6e685693f9e659ca
marcopolello <marco-pol@hotmail.it>
2020-11-18 16:30:18 +0100

    prove per modificare l'elemento gia stampato

--- js/script.js
+++ js/script.js
@@ -4,20 +4,26 @@
 var app = new Vue ({
   el: '#app',
   data: {
-    input: '', //variabile,
-    array: [], //array in cui pusho input
+    input: null, //variabile,
+    edit: false,
+    toDoList: [{'text': this.input, 'edit': this.edit}], //array in cui pusho input
   },
   methods: {
     printodo: function () {
       //al click del bottone pusho l'input nell'array
-      this.array.push(this.input);
-      console.log(this.input);
-      console.log(this.array);
+      if (!this.input) {
+        this.toDoList.push('Beato te che non hai nulla da fare');
+      } else {
+        this.toDoList.push(this.input);
+      }
       this.input = ''; //resetto il mio input
     },
     deletetodo: function (i) {
       //al click dell'icona mi cancella il list item
-      this.array.splice(i, 1);
-    }
+      this.toDoList.splice(i, 1);
+    },
+    // editodo: function(i) {
+    //   this.toDoList[i] =
+    // }
   }
 })
