5d52ea0b3c9ce29033265bfc043e4f4533fc37c8
marcopolello <marco-pol@hotmail.it>
2020-11-18 18:18:53 +0100

    to do app ok

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-// un input permette all’utente di scrivere una “cosa da fare”;
+// un toDo permette all’utente di scrivere una “cosa da fare”;
 // un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
 // una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
 var app = new Vue ({
   el: '#app',
   data: {
-    input: null, //variabile,
-    edit: false,
-    toDoList: [{'text': this.input, 'edit': this.edit}], //array in cui pusho input
+    todo: '', //variabile,
+    toDoList: [], //array in cui pusho toDo
+    newtodo: '',
   },
   methods: {
     printodo: function () {
-      //al click del bottone pusho l'input nell'array
-      if (!this.input) {
+      //al click del bottone pusho l'toDo nell'array
+      if (!this.todo) {
         this.toDoList.push('Beato te che non hai nulla da fare');
       } else {
-        this.toDoList.push(this.input);
+        this.toDoList.push(this.todo);
       }
-      this.input = ''; //resetto il mio input
+      this.todo = ''; //resetto il mio toDo
     },
     deletetodo: function (i) {
       //al click dell'icona mi cancella il list item
       this.toDoList.splice(i, 1);
     },
-    // editodo: function(i) {
-    //   this.toDoList[i] =
-    // }
+    editodo: function(i) {
+      
+    }
   }
 })
