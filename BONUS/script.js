1103639670c8d614dccb9d3f6581726f31b1f655
marcopolello <marco-pol@hotmail.it>
2020-11-19 12:55:30 +0100

    ex bonus con lodash

---
+++ BONUS/script.js
@@ -0,0 +1,50 @@
+var todoApp = new Vue({
+  el: '#todoApp',
+  data: {
+    message: 'Benvenuto in ToDo App',
+    todoInput: '',
+    lists: [],
+    hasError: false
+  },
+  computed: {
+    filterLists: function(){
+      return _.orderBy(this.lists, ['isComplete', false])
+    }
+  },
+  methods:{
+    addTask: function(){
+      // error if user non inserisce nulla
+      if(!this.todoInput){
+        this.hasError = true;
+        return;
+      }
+
+      this.hasError = false;
+
+      // pusho obj
+      this.lists.push({
+        id:this.lists.length+1,
+        title: this.todoInput,
+        isComplete: false
+      });
+
+      // reset input field.
+      this.todoInput = '';
+    },
+    // delete del todo
+    deleteTask: function (list) {
+      //al click dell'icona mi cancella il list item
+      var index = _.findIndex(this.lists, list)
+      this.lists.splice(index, 1);
+    },
+    // edit del toDo già printato
+    updateTask: function(e, list){
+      e.preventDefault();
+      list.title = e.target.innerText; //riscrivo la proprietà title riportando l'evento nella function
+      e.target.blur(); //<-- perdo il focus dall'elemento selezionato.
+    },
+    completeTask: function(list){
+      list.isComplete = !list.isComplete;
+    }
+  }
+});
